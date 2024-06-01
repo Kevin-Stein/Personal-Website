930cede06149b07f0fa787cf345ad1a37f805ecb
Kevin <kevinstein@gmx.net>
2024-06-01 15:49:20 +0200

    remove background vid

--- script/neon_cursor.js
+++ script/neon_cursor.js
@@ -3,13 +3,13 @@ import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-t
 neonCursor({
   el: document.getElementById('app'),
   shaderPoints: 16,
-  curvePoints: 80,
+  curvePoints: 100,
   curveLerp: 0.5,
   radius1: 5,
-  radius2: 30,
-  velocityTreshold: 10,
-  sleepRadiusX: 100,
-  sleepRadiusY: 100,
+  radius2: 10,
+  velocityTreshold: 100,
+  sleepRadiusX: 200,
+  sleepRadiusY: 200,
   sleepTimeCoefX: 0.0025,
   sleepTimeCoefY: 0.0025
 })
