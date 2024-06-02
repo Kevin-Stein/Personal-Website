e618d75b9524c7e57566ac8473f4d8c2a8f9aeaf
Kevin <kevinstein@gmx.net>
2024-06-02 22:38:36 +0200

    some changes

--- script/neon_cursor.js
+++ script/neon_cursor.js
@@ -1,7 +1,7 @@
 import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
 neonCursor({
-  el: document.getElementById('app'),
+  el: document.getElementById("app"),
   shaderPoints: 30,
   curvePoints: 50,
   curveLerp: 0.5,
@@ -11,5 +11,5 @@ neonCursor({
   sleepRadiusX: 200,
   sleepRadiusY: 200,
   sleepTimeCoefX: 0.0025,
-  sleepTimeCoefY: 0.0025
-})
+  sleepTimeCoefY: 0.0025,
+});
