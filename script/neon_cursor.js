34d24655a831a7a60baa7f120c3c505c2865bf35
Kevin <kevinstein@gmx.net>
2024-06-13 22:12:04 +0200

    fix cursor add mousemove effect

--- script/neon_cursor.js
+++ script/neon_cursor.js
@@ -13,3 +13,27 @@ neonCursor({
   sleepTimeCoefX: 0.0025,
   sleepTimeCoefY: 0.0025,
 });
+
+window.onload = function() {
+  const imageElement = document.querySelector('[data-js="about_me"]');
+  const body = document.querySelector('[data-js="body"]')
+  if (body) {
+    const handleMouseMove = (e) => {
+      let rect = body.getBoundingClientRect();
+      let x = e.clientX - rect.left;
+      let y = e.clientY - rect.top;
+
+      let dx = (x - rect.width / 2) / (rect.width / 2);
+      let dy = (y - rect.height / 2) / (rect.height / 2);
+
+      imageElement.style.transform = `perspective(1000px) rotateY(${dx * 5}deg) rotateX(${-dy * 5}deg)`;
+    };
+
+    const handleMouseLeave = () => {
+      imageElement.style.transform = "";
+    };
+
+    body.addEventListener('mousemove', handleMouseMove);
+    body.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
